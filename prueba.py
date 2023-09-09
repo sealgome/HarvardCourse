13507884883cb421f8acf1719112afefcab96e41
sealgome <sergiogomezagt77@gmail.com>
2023-09-09 09:49:18 -0500

    primer commit

---
+++ prueba.py
@@ -0,0 +1,11 @@
+import random 
+# Pick a random number
+random_number = random.randint(1,10)
+
+#Input from the user
+user_guess = int(input("Insert your guess number: "))
+
+if random_number == user_guess:
+    print("Well done. Your number was correct!")
+else:
+    print("Sorry, you didnt guess the number")
